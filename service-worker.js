c1b0908b7fc12770f21e9b524eaace237072d148
Jalel Belkacem <jalel.belkacem@epitech.eu>
2024-11-29 14:51:58 +0100

    [feat] ajout des étapes pour la PWA

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/main.js",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-512x512.png"
+];
+
+// Installation du service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activation du service worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
